503e356b1ddea47b170e7389f5cc8c809dfdde46
Adnan Mouhandiz <amouhand@c4r4p8.1337.ma>
2024-04-16 21:14:50 +0100

    almost done, only a bit of Makefile

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: amouhand <amouhand@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 22:59:28 by amouhand          #+#    #+#             */
-/*   Updated: 2024/04/16 02:29:12 by amouhand         ###   ########.fr       */
+/*   Updated: 2024/04/16 21:13:58 by amouhand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,29 @@ void sendbits(int pid, char c)
 {
 	int i;
 
-	i = 7;
-	while (i >= 0)
+	i = 0;
+	while (i < 8)
 	{
 		if (c & (1 << i))
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
 		usleep(100);
-		i--;
+		i++;
 	}
+	// 'c' = 01100011 | 1 = 00000001
+	
+	// 'c' = 00000001 = 1, SIGUSR1
+	// 'c' = 00000010 = 1, SIGUSR1
+	// 'c' = 00000000 = 0, SIGUSR2
+	// 'c' = 00000000 = 0, SIGUSR2
+	// 'c' = 00000000 = 0, SIGUSR2
+	// 'c' = 00100000 = 1, SIGUSR1
+	// 'c' = 01000000 = 1, SIGUSR1
+	// 'c' = 00000000 = 0, SIGUSR2
+
+	// sleep 100 microseconds to give enough time to send the signal
+	// kill is a function that sends a signal to a pid you give
 }
 
 int main(int ac, char *av[])
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: amouhand <amouhand@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 22:59:58 by amouhand          #+#    #+#             */
-/*   Updated: 2024/04/16 02:55:42 by amouhand         ###   ########.fr       */
+/*   Updated: 2024/04/16 21:12:45 by amouhand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,24 @@ void handler(int signal)
 	static int i;
 
 	if (signal == SIGUSR1)
-		i |= (1 << bits);
-	i++;
-	if (i == 8)
+		i = i | (0x01 << bits);
+	bits++;
+	if (bits == 8)
 	{
 		ft_printf("%c", i);
 		bits = 0;
 		i = 0;
 	}
+	// SIGUSR1, i = 00000000 | 00000001 = 00000001, bits = 0
+	// SIGUSR1, i = 00000001 | 00000010 = 00000011, bits = 1
+	// SIGUSR2, i = 00000011, bits = 2
+	// SIGUSR2, i = 00000011, bits = 3
+	// SIGUSR2, i = 00000011, bits = 4
+	// SIGUSR1, i = 00000011 | 00100000 = 00100011, bits = 5
+	// SIGUSR1, i = 00100011 | 01000000 = 01100011, bits = 6
+	// SIGUSR2, i = 01100011, bits = 7
+	// bits++;
+	// then print the i as character from the built binary, reset the static then continue
 }
 int main(int ac, char *av[])
 {
@@ -39,10 +49,12 @@ int main(int ac, char *av[])
 	pid = getpid();
 	ft_printf("Server PID: %d\n", pid);
 	ft_printf("Waiting for messages...\n");
-	while(ac == 1)
+	while (ac == 1)
 	{
 		signal(SIGUSR1, handler);
 		signal(SIGUSR2, handler);
+		// pause function pauses till a signal arrives
 		pause();
 	}
+	return (0);
 }
